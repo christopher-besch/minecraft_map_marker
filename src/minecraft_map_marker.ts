ddb0187963280a840ba3c148101c37c408691347
christopher-besch <mail@chris-besch.com>
2024-11-22 10:54:04 +0100

    feat: refactor; add pin to leaflet pin map

--- src/minecraft_map_marker.ts
+++ src/minecraft_map_marker.ts
@@ -1,6 +1,9 @@
 // from MinedMap
 declare var createMap: () => Promise<[L.Map, L.Layer]>;
 
+///////////
+// types //
+///////////
 type NetherCoords = {
     x: number;
     y: number | null;
@@ -13,12 +16,6 @@ type OverworldCoords = {
     z: number;
     __brand: 'OverworldCoords';
 };
-function netherToOverworldCoords(coords: NetherCoords): OverworldCoords {
-    return { x: coords.x * 8, y: coords.y, z: coords.z * 8, __brand: 'OverworldCoords' };
-}
-function overworldToNetherCoords(coords: OverworldCoords): NetherCoords {
-    return { x: Math.round(coords.x / 8), y: coords.y, z: Math.round(coords.z / 8), __brand: 'NetherCoords' };
-}
 
 type Pin = {
     coords: OverworldCoords;
@@ -26,153 +23,241 @@ type Pin = {
     description: string;
 }
 
-let overworld_x_field = document.getElementById('overworld-x') as HTMLInputElement;
-let overworld_z_field = document.getElementById('overworld-z') as HTMLInputElement
-let nether_x_field = document.getElementById('nether-x') as HTMLInputElement;
-let nether_z_field = document.getElementById('nether-z') as HTMLInputElement
-let height_field = document.getElementById('height') as HTMLInputElement
-let title_field = document.getElementById('title') as HTMLInputElement
-let description_field = document.getElementById('description') as HTMLInputElement
-let place_pin_button = document.getElementById('place-pin') as HTMLButtonElement;
+namespace Pins {
+    const PIN_STORAGE_ID = 'pins';
 
-let temp_marker = L.marker([0, 0]);
-let pin_layer_group = L.layerGroup([]);
+    // lookup of each user pin's leaflet marker
+    let leaflet_user_pins: Map<Pin, L.Marker> = new Map();
+    // lookup of each const pin's leaflet marker
+    let leaflet_const_pins: Map<Pin, L.Marker> = new Map();
 
-function getHeightInput(): number | null {
-    if (!height_field.checkValidity()) {
-        return null;
+    // the current coordinate input is reflected with this pin
+    // it isn't stored and thus temporary
+    let temp_marker = L.marker([0, 0]);
+    // the layer for the leaflet pin markers created by the user
+    let leaflet_user_pin_group = L.layerGroup([]);
+    // the layer for the leaflet pin markers set by the server
+    // the user can't change these, except for hiding all
+    let leaflet_const_pin_group = L.layerGroup([]);
+
+    function retrieveUserPinsFromStorage(): Pin[] {
+        const raw_pins = localStorage.getItem(PIN_STORAGE_ID);
+        if (raw_pins === null)
+            return [];
+        return JSON.parse(raw_pins);
     }
-    return parseInt(height_field.value) || null;
-}
-function getOverworldCoordsInput(): OverworldCoords | null {
-    if (!overworld_x_field.checkValidity() || !overworld_z_field.checkValidity()) {
-        return null;
+    function storeUserPinsToStorage(pins: Pin[]) {
+        localStorage.setItem(PIN_STORAGE_ID, JSON.stringify(pins));
     }
-    const x = parseInt(overworld_x_field.value);
-    const z = parseInt(overworld_z_field.value);
-    if (isNaN(x) || isNaN(z)) {
-        return null;
-    }
-    return { x, y: getHeightInput(), z, __brand: 'OverworldCoords' };
-}
-function getNetherCoordsInput(): NetherCoords | null {
-    if (!nether_x_field.checkValidity() || !nether_z_field.checkValidity()) {
-        return null;
-    }
-    const x = parseInt(nether_x_field.value);
-    const z = parseInt(nether_z_field.value);
-    if (isNaN(x) || isNaN(z)) {
-        return null;
-    }
-    return { x, y: getHeightInput(), z, __brand: 'NetherCoords' };
-}
-function getPinInput(): Pin | null {
-    const coords = getOverworldCoordsInput();
-    if (coords === null)
-        return null;
-
-    if (!title_field.checkValidity())
-        return null;
-    let title = title_field.value;
-    if (title === '')
-        title = 'Unnamed Pin';
-
-    if (!description_field.checkValidity())
-        return null;
-    const description = description_field.value;
-
-    return { coords, title, description };
-}
-function setPinInput(pin: Pin, map: L.Map) {
-    setCoords(pin.coords, map);
-    title_field.value = pin.title;
-    description_field.value = pin.description;
-}
-
-function setCoords(overworld_coords: OverworldCoords, map: L.Map) {
-    const nether_coords = overworldToNetherCoords(overworld_coords);
-    overworld_x_field.value = String(overworld_coords.x);
-    overworld_z_field.value = String(overworld_coords.z);
-    nether_x_field.value = String(nether_coords.x);
-    nether_z_field.value = String(nether_coords.z);
-    height_field.value = overworld_coords.y === null ? '' : String(overworld_coords.y);
-
-    temp_marker.setLatLng([-overworld_coords.z, overworld_coords.x]);
-}
-
-function initializeForm(map: L.Map) {
-    function updateCoordsFromOverworld() {
-        const coords = getOverworldCoordsInput();
-        if (coords === null)
-            return;
-        setCoords(coords, map);
-    }
-    function updateCoordsFromNether() {
-        const coords = getNetherCoordsInput();
-        if (coords === null)
-            return;
-        setCoords(netherToOverworldCoords(coords), map);
+    function retrieveConstPinsFromServer(): Pin[] {
+        // TODO: implement
+        return [];
     }
 
-    overworld_x_field.addEventListener('keyup', updateCoordsFromOverworld);
-    overworld_z_field.addEventListener('keyup', updateCoordsFromOverworld);
-    nether_x_field.addEventListener('keyup', updateCoordsFromNether);
-    nether_z_field.addEventListener('keyup', updateCoordsFromNether);
+    function addPinToMap(pin: Pin, is_const_pin: boolean) {
+        function createPinIcon(pin: Pin): L.DivIcon {
+            const icon_path = './vendor/leaflet/images/marker-icon.png';
 
-    function createPinIcon(pin: Pin): L.DivIcon {
-        const icon_path = './vendor/leaflet/images/marker-icon.png';
+            let title = document.createElement('h3');
+            title.textContent = pin.title;
+            title.className = 'pin-title';
 
-        let title = document.createElement('h3');
-        title.textContent = pin.title;
-        title.className = 'pin-title';
-
-        let img = document.createElement('img');
-        img.src = icon_path;
-        img.className = 'pin-img';
+            let img = document.createElement('img');
+            img.src = icon_path;
+            img.className = 'pin-img';
 
 
-        let icon_div = document.createElement('div');
-        icon_div.appendChild(img);
-        icon_div.className = 'pin';
-        icon_div.appendChild(title);
+            let icon_div = document.createElement('div');
+            icon_div.appendChild(img);
+            icon_div.className = 'pin';
+            icon_div.appendChild(title);
 
-        return L.divIcon({
-            html: icon_div,
-            // scale as much as is needed for the text
-            iconSize: undefined,
-            // the icon has a size of 25 x 41
-            iconAnchor: [12, 41],
-            // avoid white square
-            className: '',
-        });
-    }
-
-    function placePin() {
-        const pin = getPinInput();
-        if (pin === null) {
-            console.log('invalid input');
-            return;
+            return L.divIcon({
+                html: icon_div,
+                // scale as much as is needed for the text
+                iconSize: undefined,
+                // the icon has a size of 25 x 41
+                iconAnchor: [12, 41],
+                // avoid white square
+                className: '',
+            });
         }
-        L.marker([-pin.coords.z, pin.coords.x], {
+
+        let marker = L.marker([-pin.coords.z, pin.coords.x], {
             icon: createPinIcon(pin),
             title: pin.description,
-        }).addTo(pin_layer_group);
+        });
+        if (is_const_pin) {
+            marker.addTo(leaflet_const_pin_group);
+            leaflet_const_pins.set(pin, marker);
+        } else {
+            // is user pin
+            marker.addTo(leaflet_user_pin_group);
+            leaflet_user_pins.set(pin, marker);
+        }
     }
-    place_pin_button.addEventListener('click', placePin);
+
+    export function setTempPinCoords(coords: OverworldCoords) {
+        temp_marker.setLatLng([-coords.z, coords.x]);
+    }
+
+    export function saveUserPin(pin: Pin) {
+        addPinToMap(pin, false);
+        storeUserPinsToStorage(retrieveUserPinsFromStorage().concat([pin]));
+    }
+
+    // TODO: use with Pin Overview
+    export function getUserPins(): Iterator<Pin> {
+        return leaflet_user_pins.keys();
+    }
+    export function getConstPins(): Iterator<Pin> {
+        return leaflet_const_pins.keys();
+    }
+    export function focusPin(pin: Pin, is_const_pin: boolean) {
+        // TODO: implement
+    }
+    export function deleteUserPin(pin: Pin) {
+        // TODO: implement
+    }
+
+    export function initialize() {
+        retrieveUserPinsFromStorage().forEach(pin => { addPinToMap(pin, false); });
+        retrieveConstPinsFromServer().forEach(pin => { addPinToMap(pin, true); });
+    }
+    export function addTo(map: L.Map) {
+        temp_marker.addTo(map);
+        leaflet_user_pin_group.addTo(map);
+        leaflet_const_pin_group.addTo(map);
+    }
+    export function getControlLayers(): { [id: string]: L.Layer } {
+        return { 'Pins': leaflet_user_pin_group, 'Default Pins': leaflet_const_pin_group };
+    }
+}
+
+namespace UserInput {
+    let overworld_x_field = document.getElementById('overworld-x') as HTMLInputElement;
+    let overworld_z_field = document.getElementById('overworld-z') as HTMLInputElement
+    let nether_x_field = document.getElementById('nether-x') as HTMLInputElement;
+    let nether_z_field = document.getElementById('nether-z') as HTMLInputElement
+    let height_field = document.getElementById('height') as HTMLInputElement
+    let title_field = document.getElementById('title') as HTMLInputElement
+    let description_field = document.getElementById('description') as HTMLInputElement
+    let save_pin_button = document.getElementById('save-pin') as HTMLButtonElement;
+
+    function netherToOverworldCoords(coords: NetherCoords): OverworldCoords {
+        return { x: coords.x * 8, y: coords.y, z: coords.z * 8, __brand: 'OverworldCoords' };
+    }
+    function overworldToNetherCoords(coords: OverworldCoords): NetherCoords {
+        return { x: Math.round(coords.x / 8), y: coords.y, z: Math.round(coords.z / 8), __brand: 'NetherCoords' };
+    }
+
+    function getHeightInput(): number | null {
+        if (!height_field.checkValidity()) {
+            return null;
+        }
+        return parseInt(height_field.value) || null;
+    }
+    function getOverworldCoordsInput(): OverworldCoords | null {
+        if (!overworld_x_field.checkValidity() || !overworld_z_field.checkValidity()) {
+            return null;
+        }
+        const x = parseInt(overworld_x_field.value);
+        const z = parseInt(overworld_z_field.value);
+        if (isNaN(x) || isNaN(z)) {
+            return null;
+        }
+        return { x, y: getHeightInput(), z, __brand: 'OverworldCoords' };
+    }
+    function getNetherCoordsInput(): NetherCoords | null {
+        if (!nether_x_field.checkValidity() || !nether_z_field.checkValidity()) {
+            return null;
+        }
+        const x = parseInt(nether_x_field.value);
+        const z = parseInt(nether_z_field.value);
+        if (isNaN(x) || isNaN(z)) {
+            return null;
+        }
+        return { x, y: getHeightInput(), z, __brand: 'NetherCoords' };
+    }
+    function getTempPin(): Pin | null {
+        const coords = getOverworldCoordsInput();
+        if (coords === null)
+            return null;
+
+        if (!title_field.checkValidity())
+            return null;
+        let title = title_field.value;
+        if (title === '')
+            title = 'Unnamed Pin';
+
+        if (!description_field.checkValidity())
+            return null;
+        const description = description_field.value;
+
+        return { coords, title, description };
+    }
+
+    export function setTempCoords(overworld_coords: OverworldCoords) {
+        const nether_coords = overworldToNetherCoords(overworld_coords);
+        overworld_x_field.value = String(overworld_coords.x);
+        overworld_z_field.value = String(overworld_coords.z);
+        nether_x_field.value = String(nether_coords.x);
+        nether_z_field.value = String(nether_coords.z);
+        height_field.value = overworld_coords.y === null ? '' : String(overworld_coords.y);
+
+        Pins.setTempPinCoords(overworld_coords);
+    }
+
+    function setTempPin(pin: Pin) {
+        setTempCoords(pin.coords);
+        title_field.value = pin.title;
+        description_field.value = pin.description;
+    }
+
+    export function initialize() {
+        function updateCoordsFromOverworld() {
+            const coords = getOverworldCoordsInput();
+            if (coords === null)
+                return;
+            setTempCoords(coords);
+        }
+        function updateCoordsFromNether() {
+            const coords = getNetherCoordsInput();
+            if (coords === null)
+                return;
+            setTempCoords(netherToOverworldCoords(coords));
+        }
+
+        // only these fields change other things
+        // all other fields only affect things when then `Save Pin` button is clicked
+        overworld_x_field.addEventListener('keyup', updateCoordsFromOverworld);
+        overworld_z_field.addEventListener('keyup', updateCoordsFromOverworld);
+        nether_x_field.addEventListener('keyup', updateCoordsFromNether);
+        nether_z_field.addEventListener('keyup', updateCoordsFromNether);
+
+        function savePinFromTemp() {
+            const pin = getTempPin();
+            if (pin === null) {
+                console.log("temp pin is invalid; can't save");
+                return;
+            }
+            Pins.saveUserPin(pin);
+        }
+        save_pin_button.addEventListener('click', savePinFromTemp);
+    }
 }
 
 const initialize = async () => {
     const [map, light_layer] = await createMap();
-    temp_marker.addTo(map);
-    pin_layer_group.addTo(map);
-    L.control.layers({}, { 'Illumination': light_layer, 'Pins': pin_layer_group }).addTo(map);
+    Pins.initialize();
+    Pins.addTo(map);
+    L.control.layers({}, Object.assign({}, { 'Illumination': light_layer }, Pins.getControlLayers())).addTo(map);
 
     map.on('click', function(e) {
-        // TODO: maybe set y to null
-        setCoords({ x: Math.round(e.latlng.lng), y: getHeightInput(), z: Math.round(-e.latlng.lat), __brand: 'OverworldCoords' }, map);
+        UserInput.setTempCoords({ x: Math.round(e.latlng.lng), y: null, z: Math.round(-e.latlng.lat), __brand: 'OverworldCoords' });
     });
 
-    initializeForm(map);
+    UserInput.initialize();
 }
 
 initialize();
